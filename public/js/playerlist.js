daf6a7bc63654e4bd6ea386670d730c18b3b6ae1
Rebecca Dummit <jaboinga@gmail.com>
2016-05-26 15:38:32 +0000

    added table code without the database, added jquery as well

---
+++ public/js/playerlist.js
@@ -0,0 +1,24 @@
+/**
+ * Hiscores
+ */
+
+function createTableRow(first, last, role, id) {
+  return '<tr> <td>' + first + '</td><td>' + last + '</td><td>' + role + '</td><td>' + id + '</td></tr>';
+}
+
+/**
+ * Populate the hiscore table by retrieving top 10 scores from the DB. 
+ * Called when the DOM is fully loaded.
+ */
+function populateTable() {	
+  var table = $("#player_table tr");
+  $.get("/playerlist", function (data) {
+    var players = JSON.parse(data);
+    players.forEach(function (player) {
+      var html = createTableRow(player.first, player.last, player.role, player.id);
+      table.last().after(html);		
+    });
+  });	
+}
+
+$(populateTable);
