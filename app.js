e233cc0378c0e3da85afa6bbdc1ec5cb20b35e3d
Bobbie Cochrane <bobbiejc@us.ibm.com>
2016-05-26 11:40:33 -0400

    Upgraded the testing of local/cloud-foundry logic to work with new cfenv package

--- app.js
+++ app.js
@@ -6,41 +6,29 @@
 
 var express = require('express');
 var app = express();
-var http = require('http');
 
-var host = "localhost";
-var port = 3030;
 var cloudant = {
 		 		 url : "https://edbede34-5fac-45c8-a2a9-a066bb3d6000-bluemix:6d0fd78d822e7fa111e98b26c317ebddb5464fecf17d2731d7a0bb50ddd01c7b@edbede34-5fac-45c8-a2a9-a066bb3d6000-bluemix.cloudant.com"	 		 
 };
-if (process.env.hasOwnProperty("VCAP_SERVICES")) {
-  // Running on Bluemix. Parse out the port and host that we've been assigned.
-  var env = JSON.parse(process.env.VCAP_SERVICES);
-  var host = process.env.VCAP_APP_HOST;
-  var port = process.env.VCAP_APP_PORT;
-
-  // Also parse out Cloudant settings.
-  cloudant = env['cloudantNoSQLDB'][0].credentials;  
-}
-var nano = require('nano')(cloudant.url);
-var db = nano.db.use('assassin');
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
-// create a new express server
-var app = express();
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// Parse out Cloudant settings
+
+if (!appEnv.isLocal) {
+   cloudant = appEnv.getServices()['cloudantNoSQLDB'][0].credentials;
+}
+
+var nano = require('nano')(cloudant.url);
+var db = nano.db.use('assassin');
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
