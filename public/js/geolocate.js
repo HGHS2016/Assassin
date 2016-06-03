ac0d9dcd636dc7e1b69cfc4f0ac76c9acf139f53
foobar42fsm <webelliot@fordcochrane.com>
2016-06-03 14:54:03 -0400

    got a basic geologger working

--- public/js/geolocate.js
+++ public/js/geolocate.js
@@ -9,7 +9,18 @@ function getLocation(callback) {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
-          
+
 function showPosition(position) {
     x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
+
+function logPosition(position) {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function showPosition(position) {
+        document.getElementById("location").value = position.coords.latitude + " N " + position.coords.longitude + " S ";
+        callback(position);
+});
+  } else {
+      x.innerHTML = "Geolocation is not supported by this browser.";
+  }
 }
