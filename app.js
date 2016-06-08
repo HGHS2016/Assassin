b3a4c309b0447d0073702adb12f5406258267a63
Bobbie Cochrane <bobbiejc@us.ibm.com>
2016-06-08 12:45:12 -0400

    enabling https on bluemix so geolocation works

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ var app = express();
 var session = require('client-sessions');
 var pass = "";
 
+function requireHTTPS(req, res, next) {
+    if (req.headers && req.headers.$wssp === "80") {
+	return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
+app.use(requireHTTPS);
+
 //var http = require('http')
 
 //var host = "localhost"
