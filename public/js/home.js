773f2fb7d986ec597900203e283efe6b85e48e75
Bobbie Cochrane <bobbiejc@us.ibm.com>
2016-06-08 14:34:53 -0400

    fixed the target page, when was this crashing?

--- public/js/home.js
+++ public/js/home.js
@@ -9,24 +9,17 @@ function populateMyTarget() {
     var req = "/mytarget?user=" + local_data.user;
 
     //added res, not sure why this worked
-    $.get(req, res, function(data) {
+    $.get(req, function(data) {
 	var target = JSON.parse(data);
-	if (target != "") {
+	if (target && target != "") {
 	    var html = '';
-	    if (target)
-	    {
-		console.log(JSON.stringify(target));
-		var targetstr = '<tr><td> Target Team: </td><td>' + target.name + '</td></tr>';
-		var player2 = '<tr><td> Player 2: </td><td> ' + target.player2.name + '</td></tr>';
-		var player1 = '<tr><td> Player 1: </td><td> ' + target.player1.name + '</td></tr>';
-		html = targetstr.concat(player1).concat(player2);
-		span.last().after(html);
-		console.log(html);
-	    }
-	}
-	else {
-		//doesn't redirect, but stops server from crashing...
-		res.redirect('/signup');
+	    console.log(JSON.stringify(target));
+	    var targetstr = '<tr><td> Target Team: </td><td>' + target.name + '</td></tr>';
+	    var player2 = '<tr><td> Player 2: </td><td> ' + target.player2.name + '</td></tr>';
+	    var player1 = '<tr><td> Player 1: </td><td> ' + target.player1.name + '</td></tr>';
+	    html = targetstr.concat(player1).concat(player2);
+	    span.last().after(html);
+	    console.log(html);
 	}
     });
 }
